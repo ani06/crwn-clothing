eafd2a46ab9c6ed4dc165ba9f345b96ad2f64a98
Anirudh Mohan <m.anirudh1992@gmail.com>
2019-10-02 02:57:26 +0530

    Updates

---
+++ precache-manifest.114b5914c36a2e09243e0cbb1a946330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4961fa4626492894501267d8c71c765d",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "ba523358f57a0b763db3",
+    "url": "/crwn-clothing/static/css/main.fe15424d.chunk.css"
+  },
+  {
+    "revision": "a233c3e6bef27db1efe1",
+    "url": "/crwn-clothing/static/js/2.fa006742.chunk.js"
+  },
+  {
+    "revision": "ba523358f57a0b763db3",
+    "url": "/crwn-clothing/static/js/main.7db18932.chunk.js"
+  },
+  {
+    "revision": "afe9836c998bceeb5fbf",
+    "url": "/crwn-clothing/static/js/runtime-main.675f06d2.js"
+  }
+]);
