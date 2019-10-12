fdedae1ebb15111b8cd24e05637bd0fab648b267
Anirudh Mohan <m.anirudh1992@gmail.com>
2019-10-12 08:05:30 +0530

    Integrate firebase into application, added styling for sign in buttons

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config= {
+    apiKey: "AIzaSyBbrB7HMn-neSQqxUv30jjgrtfHhZLBeXM",
+    authDomain: "crwn-db-4bb3d.firebaseapp.com",
+    databaseURL: "https://crwn-db-4bb3d.firebaseio.com",
+    projectId: "crwn-db-4bb3d",
+    storageBucket: "crwn-db-4bb3d.appspot.com",
+    messagingSenderId: "416233308594",
+    appId: "1:416233308594:web:3c24d802ece6cd584d64ab",
+    measurementId: "G-9RDZN5DXND"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt : 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
